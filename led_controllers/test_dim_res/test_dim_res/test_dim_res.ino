04aa97c93e206075fe0b39305dee18219b31832e
Janosch Moor <janosch.moor@gmail.com>
2024-04-23 12:13:18 +0200

    dimming trail effect

--- led_controllers/test_dim_res/test_dim_res/test_dim_res.ino
+++ led_controllers/test_dim_res/test_dim_res/test_dim_res.ino
@@ -2,6 +2,13 @@
 #define NUM_LEDS 150
 CRGB leds[NUM_LEDS];
 
+/*
+
+Research shows that its likely that the update frequency of ws2812 is around 400 hz corresponding to an update 
+every 2.5 ns
+
+*/
+
 float brightness[NUM_LEDS];
 int id = 10;
 
@@ -13,17 +20,31 @@ void loop() {
   FastLED.setBrightness(100);
   int dir = 1;
 
+  CRGB target = CRGB::Plum;
+  CRGB fadeOut = CRGB::Black;
+
   while(true) {
+
+    id += dir;
     if (id == NUM_LEDS-1 || id == 0) {
       dir *= -1;
     }
-    id += dir;
-    if (dir == 1) {
-      leds[id] = CRGB::Purple;
-    } else {
-      leds[id] = CRGB::Black;
+
+    brightness[id] = 1.0;
+    for (int j = 0; j < 5; j++) {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        if (brightness[i] < 0.1) {
+          leds[i] = CRGB::Black;
+        } else {
+          leds[i] = blend(target, fadeOut, floor(255 * (1 - brightness[i])));
+        }
+        brightness[i] *= 0.99;
+      }
+
+      FastLED.show();
+      delay(3);
     }
-    FastLED.show();
-    delay(50);
+
+
   }
 }
