566e61d4f9aa165809c3721f7b8437ee980e31e1
Janosch Moor <janosch.moor@gmail.com>
2024-04-30 09:20:43 +0200

    idea

--- led_controllers/sunrise/sunrise.ino
+++ led_controllers/sunrise/sunrise.ino
@@ -1,3 +1,10 @@
+// the idea of this file is to build a lights animation that runs on < 150 leds, where over the period of 30 minutes 3 dots spawn:
+// yellow, red to blue or something. they move slowly and can blend inbetween pixels (subpixel rendering)
+// when they move along the strip they pull the led target value closer to their own which chould also be a way
+// of morphing between their own values. 
+// this strip will be mounted under the bed ;)
+
+
 
 void setup() {
   // put your setup code here, to run once:
