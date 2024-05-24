94c6c4d48947a3763c1f003825aef6e68592a072
Janosch Moor <janosch.moor@gmail.com>
2024-05-24 14:52:57 +0200

    refactoring

---
+++ led_controllers/sunrise/main/ButtonController.h
@@ -0,0 +1,19 @@
+#ifndef BUTTONCONTROLLER_H
+#define BUTTONCONTROLLER_H
+
+#include <Arduino.h>
+
+class ButtonController {
+public:
+    // Constructor(s) and destructor
+    ButtonController();
+    ~ButtonController();
+
+    // Declare your member functions here
+
+private:
+    // Declare any private member variables here
+
+};
+
+#endif
